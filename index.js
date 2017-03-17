ef4817821476590dbaee8e44f059a11bd514e27a
Steve Gengler <steve@o3world.com>
2017-03-17 12:40:50 -0400

    formatting config and updatign sample eslint file

--- index.js
+++ index.js
@@ -13,14 +13,36 @@ module.exports = {
     "eol-last": "off",
     "func-names": "off",
     "guard-for-in": "off",
-    "max-len": ["error", { "ignorePattern": "^(function|.factory|.service|.controller|.config|.run|.filter|.component|contstructor)" }],
     "keyword-spacing": "warn",
+    "max-len": [
+      "error", {
+        "ignorePattern": "^(function|.factory|.service|.controller|.config|.run|.filter|.component|contstructor)"
+      }
+    ],
     "no-console": "warn",
-    "no-multiple-empty-lines": ["error", { "max": 4, "maxEOF": 2 }],
-    "no-param-reassign": ["error", { "props": false }],
+    "no-multiple-empty-lines": [
+      "error", {
+        "max": 4,
+        "maxEOF": 2
+      }
+    ],
+    "no-param-reassign": [
+      "error", {
+        "props": false
+      }
+    ],
     "no-restricted-syntax": "warn",
-    "no-trailing-spaces": ["error", { "skipBlankLines": true }],
-    "no-use-before-define": ["error", { "functions": false, "classes": false } ],
+    "no-trailing-spaces": [
+      "error", {
+        "skipBlankLines": true
+      }
+    ],
+    "no-use-before-define": [
+      "error", {
+        "functions": false,
+        "classes": false
+      }
+    ],
     "padded-blocks": "off"
   }
 }
